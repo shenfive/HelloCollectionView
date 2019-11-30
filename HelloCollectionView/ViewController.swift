a9c867fa7f225f9bf72fa09523a335af45fc1244
Danny Shen <shenfive@gmail.com>
2019-11-30 13:33:06 +0800

    init

--- HelloCollectionView/ViewController.swift
+++ HelloCollectionView/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    
+    @IBOutlet weak var myCC: UICollectionView!
+    
+    var images:[UIImage?] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        images = [
+            UIImage(named: "png1"),
+            UIImage(named: "png2"),
+            UIImage(named: "png3"),
+            UIImage(named: "png4"),
+            UIImage(named: "png5"),
+        ]
+        myCC.delegate = self
+        myCC.dataSource = self
     }
-
-
+    
+    //MARK: CollectionView Delegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ccCell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    
 }
 
